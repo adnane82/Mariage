67bda6aaacaee591baee9490ceff80652f4c410d
mantie <adnane.elghazi1@gmail.com>
2020-01-08 18:55:00 +0100

    EndOfSection 3

---
+++ ZwajApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZwajApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [StringLength(8,MinimumLength=4,ErrorMessage="Votre mot de passe n'est pas correcte")]
+        public string Password { get; set; }
+
+    }
+}
